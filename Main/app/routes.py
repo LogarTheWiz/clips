52e79c6fad597fdfb505dca515197cb7887aa0e6
LogarTheWiz <150455951+LogarTheWiz@users.noreply.github.com>
2025-02-27 08:22:37 +0000

    documentation

--- Main/app/routes.py
+++ Main/app/routes.py
@@ -5,3 +5,7 @@ routes = Blueprint('routes', __name__)
 @routes.route("/", methods=["GET"])
 def home():
     return render_template("routes/home.html")
+
+@routes.route("/documentation", methods=["GET"])
+def documentation():
+    return render_template("routes/documentation.html")
